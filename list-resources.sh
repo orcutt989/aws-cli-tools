e5d69949e908778cc8df971443899fb857085687
orcutt989 <james.orcutt@puppet.com>
2019-05-16 13:52:46 -0400

    List VPCs in all regions.

--- list-resources.sh
+++ list-resources.sh
@@ -2,4 +2,6 @@ for region in `aws ec2 describe-regions --output text | cut -f3`
 do
      echo -e "\nListing Instances in region:'$region'..."
      aws ec2 describe-instances --query "Reservations[*].Instances[*].{IP:PublicIpAddress,ID:InstanceId,Type:InstanceType,State:State.Name,Name:Tags[0].Value}" --output=table --region $region
+     echo -e "\nListing VPCs in region:'$region'..."
+     aws ec2 describe-vpcs --query "Reservations[*].Instances[*].{IP:PublicIpAddress,ID:InstanceId,Type:InstanceType,State:State.Name,Name:Tags[0].Value}" --output=table --region $region
 done
